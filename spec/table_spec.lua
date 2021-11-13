725aeecbdef1e43ff26b23400cdbff2e11d291e3
lalawue <suchaaa@gmail.com>
2021-11-14 00:58:20 +0800

    improve table key/value and comment interleave

--- spec/table_spec.lua
+++ spec/table_spec.lua
@@ -76,7 +76,60 @@ describe("test success 2 #table", function()
         assert.is_true(ret)
         assert.is_true(type(content) == "string")
     end)
- 
+
+    local f = load(content, "test", "t")
+    it("should get function", function()
+        assert(type(f) == "function")
+        local t = f(10)
+        assert.is_equal(t.c, 11)
+        assert.is_equal(t.b, 19)
+        assert.is_equal(t.d, 3)
+    end)
+end)
+
+describe("test comment #table", function()
+    local mnstr=[[
+        b = 9 + ...
+        a =  {
+            --- 111
+            --- 222
+             3,
+             4 = 3, --- 222
+             --[=[ 1231232 ]=]
+             --[=[ 212312 ]=]
+             fn(){} = 4,
+             {fn(){}} = fn(){}, --[=[ 90123d ]=]
+             false = tonumber("10"),
+             class = 12,
+             c = 1 + ...,
+             =b,
+             d = b,
+             [b] = b
+             -- 999
+             -- 33
+             --[=[ 222 ]=]
+        }
+        c = {
+            -- 111
+            -- 222
+            --[=[ 12312 ]=]
+        }
+        a [ "d"] = 3
+        return a
+    ]]
+
+    local ret, ast = parser.parse(mnstr)
+    it("should get ast", function()
+        assert.is_true(ret)
+        assert.is_true(type(ast) == "table")
+    end)
+
+    local ret, content = compile.compile({}, ast)
+    it("should get compiled lua", function()
+        assert.is_true(ret)
+        assert.is_true(type(content) == "string")
+    end)
+
     local f = load(content, "test", "t")
     it("should get function", function()
         assert(type(f) == "function")
